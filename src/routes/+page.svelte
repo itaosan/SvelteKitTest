22afefedd0f9b2cc9be42b0c54112d9bba7f26f3
shusaku.tomihisa <shusaku@monicle.co.jp>
2023-07-25 17:25:15 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Newt・SvelteKitブログ</title>
+	<meta name="description" content="NewtとSvelteKitを利用したブログです" />
+</svelte:head>
+
+<main>
+	<ul>
+		{#each data.articles as article (article._id)}
+			<li>
+				<a href={`articles/${article.slug}`}>{article.title}</a>
+			</li>
+		{/each}
+	</ul>
+</main>
